6936b75331ce38a73b2866220a259fade8019174
Artur Ribeiro <profissional.arturrc@gmail.com>
2022-10-23 18:27:12 -0300

    Curso em andamento

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,59 @@
+import { Container } from './styles'
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
+
+import { NavLink } from 'react-router-dom'
+
+import MenuImg from '../../assets/menu.svg'
+import { useState } from 'react'
+
+export const SideBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
+        <img src={MenuImg} alt='Botão de expandir seleção' />
+      </button>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/'>
+              <BurgerIcon />
+              <span>Hamburgeres</span>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to='pizza'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to='drinks'>
+              <SodaPopIcon />
+              <span>Refrigerantes</span>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to='ice-creams'>
+              <IceCreamIcon />
+              <span>Sorvetes</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/Main'
+import { Burgers } from './pages/Main/Burgers'
+import { Pizza } from './pages/Main/Pizza'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />}>
+        <Route path='/' element={<Burgers />} />
+        <Route path='pizza' element={<Pizza />} />
+      </Route>
     </Routes>
   )
 }
